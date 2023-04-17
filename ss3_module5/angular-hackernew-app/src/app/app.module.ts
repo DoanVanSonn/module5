2aae124987ca9541f491a6e3db3d4ef3841d3ec4
DoanVanSonn <sonmonter123@gmai.com>
2023-04-17 15:03:31 +0700

    ss4

---
+++ ss3_module5/angular-hackernew-app/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ArticalComponent } from './component/artical/artical.component';
+import { LikeComponent } from './component/like/like.component';
+import {FormsModule} from "@angular/forms";
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { FooterComponent } from './component/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ArticalComponent,
+    LikeComponent,
+    NavbarComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
